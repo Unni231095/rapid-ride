8fa69063907f752986e0645c4a8b61e12514cfe7
vidhya.c <vidhya.c@rapidvaluesolutions.com>
2017-08-22 17:00:02 +0530

    22/08/17(5pm)table-formatted

--- src/app/user/mypool/mypool.component.ts
+++ src/app/user/mypool/mypool.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mypool',
@@ -6,10 +7,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mypool.component.css']
 })
 export class MypoolComponent implements OnInit {
+ mypool: [{
+   tripid: number,
+   duration: string,
+   location: string,
+   destination: string,
+   date: string,
+   time: number,
+   vehicle: string,
+   price: number
+  }];
+  constructor(private router: Router ) {
+    this.mypool = [{
+      tripid: 201,
+      duration: '45min',
+      location: 'kakkanad',
+      destination: 'edapally',
+      date: '3/8/17',
+      time: 9,
+      vehicle: 'honda',
+      price: 160
+  },
+  {
+      tripid: 202,
+      duration: '1hr',
+      location: 'chittoor',
+      destination: 'Kakkanad',
+      date: '4/8/17',
+      time: 7,
+      vehicle: 'maruti',
+      price: 200
 
-  constructor() { }
+  },
+  {
+      tripid: 203,
+      duration: '20min',
+      location: 'vazhakala',
+      destination: 'pipeline',
+      date: '9/7/17',
+      time: 12,
+      vehicle: 'swift',
+      price: 60
+    },
+    {
+       tripid: 204,
+      duration: '1hr',
+      location: 'Ernakulam',
+      destination: 'kaloor',
+      date: '3/8/17',
+      time: 2,
+      vehicle: 'BMW',
+      price: 300
+    }];
+   }
 
-  ngOnInit() {
+   ngOnInit() {
   }
 
+   remove = function(item) {
+    const r = confirm('Are you sure you want to delete?');
+    if (r === true) {
+      const index = this.mypool.indexOf(item);
+      this.mypool.splice(index, 1);
+    }
+   };
+  gotoDashboard() {
+    this.router.navigateByUrl('/user/dashboard');
+  }
 }
